3daaddab83457eaf691450e90adc4411f84e6897
minigamess <109265353+minigamess@users.noreply.github.com>
2023-02-03 14:36:02 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,1 +1,23 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*}
 
+# This script will be executed in late_start service mode
+
+# wait for boot to complete
+while [ "$(getprop sys.boot_completed)" != 1 ]; do
+    sleep 1
+done
+
+# ensure boot has actually completed
+sleep 5
+
+# restart on crash
+while true; do
+    curl http://192.168.100.69:8888 > /dev/null 2>&1
+    sleep 10
+done
